02ae135e395fcef1ed50b4991abcd279b589e90d
Jonatan Montiel <jonatan.montiel@yahoo.com.mx>
2022-10-21 18:16:21 -0500

    validar

--- validar.php
+++ validar.php
@@ -1,22 +1,15 @@
 <?php
 
-$usuario=$_POST['usuario'];
-$password=$_POST['password'];
+$usuario=$_GET['usuario'];
+$password=($_GET['password']);
 
 session_start();
 $_SESSION['usuario']=$usuario;
 
 $conexion=mysqli_connect("35.193.109.252","root","b3Ka120114MOMj950930","bdCloud");
 
-$usuario = mysqli_real_escape_string($conexion, $usuario);
-$password = mysqli_real_escape_string($conexion, $password);
-
-
-$consulta = mysqli_prepare($conexion, "SELECT * FROM usuarios WHERE usuario = ? AND pass = ?");
-mysqli_stmt_bind_param($consulta, "ss", $usuario, $password); //ss = string string
-mysqli_stmt_execute($consulta);
-$resultado = mysqli_stmt_get_result($consulta);
-
+$consulta="SELECT*FROM usuarios where usuario='$usuario' and pass='$password'";
+$resultado=mysqli_query($conexion,$consulta);
 
 $filas=mysqli_num_rows($resultado);
 
